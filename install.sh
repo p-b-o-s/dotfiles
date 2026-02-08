06df1e9c4ba23cfdfdb94ff4f1053b756586c571
p-b-o-s <197465153+p-b-o-s@users.noreply.github.com>
2026-02-08 17:25:46 +0000

    update: install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
+
+echo "🏠 Symlink dotfiles..."
 # Symlink to Home
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -sf ~/dotfiles/.config/nvchad ~/.config/nvchad
 
+echo "🏠 Symlink dotfiles..."
 cp ~/dotfiles/.config/opencode/opencode.json ~/.config/opencode/opencode.json
 cp ~/dotfiles/.config/mcp/mcp_servers.json ~/.config/mcp/mcp_servers.json
-cp ~/dotfiles/.config/claude ~/.claude
+cp -r ~/dotfiles/.config/claude ~/.claude
