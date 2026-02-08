21daf71b8ebe8cd6d8c6601c8e4fe09170725f3f
p-b-o-s <197465153+p-b-o-s@users.noreply.github.com>
2026-02-08 17:09:16 +0000

    update: install.sh

--- install.sh
+++ install.sh
@@ -3,3 +3,7 @@
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -sf ~/dotfiles/.config/nvchad ~/.config/nvchad
+
+cp ~/dotfiles/.config/opencode/opencode.json ~/.config/opencode/opencode.json
+cp ~/dotfiles/.config/mcp/mcp_servers.json ~/.config/mcp/mcp_servers.json
+cp ~/dotfiles/.config/claude ~/.claude
